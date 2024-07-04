9d9b4cd5df7fef538f45d886e498fb99e4d7b7ca
abdoomr2003 <abdoomer1112003@gmail.com>
2024-07-05 00:06:38 +0300

    finish

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,7 +10,7 @@ import mysql.connector
 from typing import List
 import logging
 
-PII_FIELDS = ["name", "email", "phone", "ssn", "password"]
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -35,7 +35,7 @@ class RedactingFormatter(logging.Formatter):
         """
 
     REDACTION = "***"
-    FORMAT = "[%(name)s] %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
