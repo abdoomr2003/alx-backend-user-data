2b462b842310cc9656997e51b4e4e9ef97ab0ca2
abdoomr2003 <abdoomer1112003@gmail.com>
2024-07-12 02:01:45 +0300

    commit

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,75 +1,97 @@
 #!/usr/bin/env python3
 """
-task 1: Error handler: Unauthorized
+Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from typing import Tuple, Any
-from flask_cors import (CORS, cross_origin)
 from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
 import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-AUTH_TYPE = os.getenv("AUTH_TYPE")
-if AUTH_TYPE == "auth":
+auth_type = getenv('AUTH_TYPE')
+if auth_type == 'auth':
     from api.v1.auth.auth import Auth
-
     auth = Auth()
-elif AUTH_TYPE == "basic_auth":
+if auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
-
     auth = BasicAuth()
-elif AUTH_TYPE == "session_auth":
+if auth_type == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
-
     auth = SessionAuth()
 
 
-@app.errorhandler(401)
+@app.before_request
+def before_request() -> None:
+    """Method to handle authentication
+    and authorization before each request.
+
+    This method ensures that each request is properly
+    authenticated and authorized before proceeding.
+    It performs the following checks:
+
+    1. If `auth` is None, the function returns immediately,
+    allowing the request to proceed.
+    2. If the request path is not in the list of excluded paths,
+    the function proceeds with authorization checks.
+    3. If the `Authorization` header is missing from the request,
+    a 401 Unauthorized response is returned.
+    4. If the current user cannot be identified from the request,
+    a 403 Forbidden response is returned.
+
+    The list of excluded paths allows certain endpoints
+    to be accessible without authentication.
+
+    Returns:
+        None
+    """
+    if auth is None:
+        return
+    request.current_user = auth.current_user(request)
+    if not auth.require_auth(
+        request.path,
+        [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
+        ]
+    ):
+        return
+    if auth.authorization_header(request) is None:
+        if auth.session_cookie(request) is None:
+            abort(401)
+    request.current_user = auth.current_user(request)
+    if auth.current_user(request) is None:
+        abort(403)
+
+
+@app.errorhandler(404)
 def not_found(error) -> str:
-    """ handler not found
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(e) -> str:
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def noacess(error) -> Tuple[Any, int]:
-    """
-        Your access is not authorized
-    Args:
-        error: Error code
-
-    Returns:
-      A string for the error
+def forbidden(e) -> str:
+    """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
-
-
-@app.before_request
-def before_request():
-    """
-    Each request is handled with its proper route
-    """
-    if auth is None:
-        pass
-    else:
-        excluded = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
-        ]
-        if auth.require_auth(request.path, excluded):
-            if auth.authorization_header(request) is None:
-                abort(401, description="Unauthorized")
-            if auth.current_user(request) is None:
-                abort(403, description="Forbidden")
-    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,14 +1,45 @@
 #!/usr/bin/env python3
-"""class SessionAuth
+""" Module of SessionAuth config
 """
-
 from api.v1.auth.auth import Auth
+from uuid import uuid4
+from typing import TypeVar
+from models.user import User
 
 
 class SessionAuth(Auth):
-    """_summary_
-
-    Args:
-        Auth (_type_): _description_
+    """  class SessionAuth that inherits from Auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user_id
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ returns a User ID based on a Session ID
+        """
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        user_id = self.user_id_by_session_id.get(session_id)
+        return user_id
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ returns a User instance based on a cookie valu
+        """
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        return User.get(user_id)
